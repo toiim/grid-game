e84d3e4e7a59816f353b5f8cd7d96150ba12292c
Tom Parke <tom.parke15@gmail.com>
2024-10-01 23:53:08 +0900

    create example to test action calling

--- src/state-machines/testMachine.ts
+++ src/state-machines/testMachine.ts
@@ -1,14 +1,23 @@
-import { createMachine, setup } from 'xstate'
+import { createMachine, setup, assign } from 'xstate'
 
-const subState = createMachine({
+const subState = setup({
+  actions: {
+    addName: assign({
+      name: (_, params) => params
+    })
+  }
+}).createMachine({
   initial: 'hello',
+  context: {
+    name: undefined
+  },
   states: {
     hello: {
       entry: () => {
         console.log('substate machine is running...')
       },
       on: {
-        CONVERSE: 'conversation',
+        CONVERSE: { target: 'conversation', actions: { type: 'addName', params: () => 'tim' } },
         REJECT: 'departure'
       }
     },

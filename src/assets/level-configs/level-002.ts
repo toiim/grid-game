db4d02c3b902edafab9791982e0f9ad2818255bd
Tom Parke <tom.parke15@gmail.com>
2024-10-23 13:28:05 +0900

    new characters

--- src/assets/level-configs/level-002.ts
+++ src/assets/level-configs/level-002.ts
@@ -16,7 +16,45 @@ export const level002: LevelConfig = {
         name: 'blue',
         stamina: 3,
         maxStamina: 3,
-        skills: ['fire-blast', 'heal'],
+        skills: ['fire-blast'],
+        defense: 10,
+        strength: 10,
+        health: 10,
+        maxHealth: 10,
+        speed: 10,
+        status: [],
+        magicSkill: 50
+      }
+    },
+    {
+      x: 0,
+      y: 3,
+      entity: {
+        teamId: 'good',
+        level: 1,
+        name: 'red',
+        stamina: 3,
+        maxStamina: 3,
+        skills: ['heal'],
+        defense: 10,
+        strength: 10,
+        health: 10,
+        maxHealth: 10,
+        speed: 10,
+        status: [],
+        magicSkill: 50
+      }
+    },
+    {
+      x: 4,
+      y: 4,
+      entity: {
+        teamId: 'bad',
+        level: 1,
+        name: 'purple',
+        stamina: 3,
+        maxStamina: 3,
+        skills: ['freeze'],
         defense: 10,
         strength: 10,
         health: 10,
@@ -33,6 +71,44 @@ export const level002: LevelConfig = {
         teamId: 'bad',
         level: 1,
         name: 'white',
+        stamina: 3,
+        maxStamina: 3,
+        skills: ['slash'],
+        defense: 10,
+        strength: 10,
+        health: 10,
+        maxHealth: 10,
+        speed: 10,
+        status: [],
+        magicSkill: 50
+      }
+    },
+    {
+      x: 2,
+      y: 2,
+      entity: {
+        teamId: 'good',
+        level: 1,
+        name: 'pink',
+        stamina: 3,
+        maxStamina: 3,
+        skills: ['slash'],
+        defense: 10,
+        strength: 10,
+        health: 10,
+        maxHealth: 10,
+        speed: 10,
+        status: [],
+        magicSkill: 50
+      }
+    },
+    {
+      x: 2,
+      y: 3,
+      entity: {
+        teamId: 'bad',
+        level: 1,
+        name: 'jacket-blood',
         stamina: 3,
         maxStamina: 3,
         skills: ['slash'],
